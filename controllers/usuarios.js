e9444fd18dbc17ba0749d645794729739ca62690
Jesus Antonio Quintero <jquinterocard@gmail.com>
2021-04-05 22:14:02 +0200

    primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,48 @@
+
+const { request, response } = require('express');
+
+const usuariosGet = (req = request, res = response) => {
+
+    const { q, nombre = 'no name', apikey } = req.query;
+
+    res.json({
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey
+    })
+}
+
+const usuariosPost = (req, res) => {
+
+    const { nombre, edad } = req.body;
+
+    res.json({
+        msg: 'post API - controlador',
+        nombre,
+        edad
+    })
+};
+
+const usuariosPut = (req, res) => {
+    const { id } = req.params;
+
+    res.json({
+        msg: 'put API - controlador',
+        id
+    })
+};
+
+const usuariosDelete = (req, res) => {
+    res.json({
+        msg: 'delete API - controlador'
+    })
+};
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosDelete
+}
